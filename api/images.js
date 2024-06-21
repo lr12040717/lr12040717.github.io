dc5162ed66c879095f68a333b3739d9d01ae0fce
lr12040717 <112813073+lr12040717@users.noreply.github.com>
2024-06-21 10:03:58 +0800

    Update images.js

--- api/images.js
+++ api/images.js
@@ -3,5 +3,8 @@
   	"msg": "",
   	"data": {
     		"url": "https://lr12040717.github.io/styles/logo/logo.png"
-  	}
+  	},
+    "lr_images": {
+    		"url": "https://lr12040717.github.io/styles/logo/logo.png"
+  	},
 }
