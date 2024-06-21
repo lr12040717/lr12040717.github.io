3305d96687fb8ed482f8b4d782033d2918d88851
lr12040717 <112813073+lr12040717@users.noreply.github.com>
2024-06-21 10:00:41 +0800

    Update images.js

--- api/images.js
+++ api/images.js
@@ -4,9 +4,5 @@
   	"data": {
     		// 测试
     		"url": "https://lr12040717.github.io/styles/logo/logo.png"
-  	},
-  	"lrimages" {
-    		// 凌然
-    		"url": "https://lr12040717.github.io/styles/logo/logo.png"
   	}
 }
