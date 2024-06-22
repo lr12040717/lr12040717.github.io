0e8c5a812a3c000342cc178cc00562f22981fb73
lr12040717 <112813073+lr12040717@users.noreply.github.com>
2024-06-22 14:51:24 +0800

    Update images.js

--- api/images.js
+++ api/images.js
@@ -1,15 +1,16 @@
 {
-    "code": 0,
-	  "msg": "你大爷的、滚！",
-	  "data": [{
-		    "id": "1",
-		    "name": "LOGO",
-		    "images": "/styles/logo/logos.png",
-		    "display": "true"
-	  },{
-		    "id": "2",
-		    "name": "logo",
-		    "images": "/styles/logo/logos.png",
-		    "display": "false"
-	  }]
+	"code": 0,
+	"msg": "你大爷的、滚！",
+	"data": [{
+		"id": "1",
+		"name": "LOGO",
+		"images": "/styles/logo/logos.png",
+		"display": "true"
+	},{
+		"id": "2",
+		"name": "logo",
+		"images": "/styles/logo/logos.png",
+		"display": "false"
+	}],
+	"logo": "/styles/logo/logos.png",
 }  
