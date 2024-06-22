8b02a641e53e1a6190a53d3974a2660911fde57b
lr12040717 <112813073+lr12040717@users.noreply.github.com>
2024-06-22 14:39:58 +0800

    Update images.js

--- api/images.js
+++ api/images.js
@@ -1,12 +1,39 @@
 {
-  	"code": 0,
-  	"msg": "",
-  	"data": [
-        {
-          "url": "https://lr12040717.github.io/styles/logo/logo.png"
-  	    },
-        {
-          "url": "https://lr12040717.github.io/styles/logo/logo.png"
-  	    }
-    ],
-}
+  "code": 0
+  ,"msg": ""
+  ,"count": 3000000
+  ,"data": [{
+    "id": "10001"
+    ,"username": "杜甫"
+    ,"email": "xianxin@baidu.com"
+    ,"sex": "男"
+    ,"city": "浙江杭州"
+    ,"sign": "点击此处，显示更多。当内容超出时，点击单元格会自动显示更多内容。"
+    ,"experience": "116"
+    ,"ip": "192.168.0.8"
+    ,"logins": "108"
+    ,"joinTime": "2016-10-14"
+  },{
+    "id": "10003"
+    ,"username": "王勃"
+    ,"email": "xianxin@baidu.com"
+    ,"sex": "男"
+    ,"city": "浙江杭州"
+    ,"sign": "人生恰似一场修行"
+    ,"experience": "65"
+    ,"ip": "192.168.0.8"
+    ,"logins": "106"
+    ,"joinTime": "2016-10-14"
+  }, {
+    "id": "10004"
+    ,"username": "李清照"
+    ,"email": "xianxin@baidu.com"
+    ,"sex": "女"
+    ,"city": "浙江杭州"
+    ,"sign": "人生恰似一场修行"
+    ,"experience": "666"
+    ,"ip": "192.168.0.8"
+    ,"logins": "106"
+    ,"joinTime": "2016-10-14"
+  }]
+}  
