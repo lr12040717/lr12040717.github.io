cd021ac34f9d3c1d3f1b84186a9cbafa871ccdaf
lr12040717 <112813073+lr12040717@users.noreply.github.com>
2024-06-22 15:12:14 +0800

    Update memberInfo.js

--- api/memberInfo.js
+++ api/memberInfo.js
@@ -1,1 +1,20 @@
-
+{							
+	"code":0,
+	"msg":"",
+	"count":"0",
+	"data":{
+		"id":1,
+		"account":"lr12040717.github.io",
+		"pass":"lr12040717.github.io",
+		"name":"凌然",
+		"status":"true",
+		"phone":"18999999999",
+		"sex":"男",
+		"age":888,
+		"identitycard":"888888888888888888",
+		"Rank":1,
+		"address":"中国",
+		"statusname":"正常",
+		"img":"/styles/logo/logos.png",
+	},
+}
