7a437ce8829a6ab521409043700a8f0cd440a03a
lr12040717 <112813073+lr12040717@users.noreply.github.com>
2024-06-25 15:26:45 +0800

    Update lrAll.js

--- layui/layuiadmin/modules/lrAll.js
+++ layui/layuiadmin/modules/lrAll.js
@@ -11,38 +11,38 @@ layui.define(function(exports){
     ,view = layui.view
     ,admin = layui.admin
     ,form = layui.form
-
+	
     function loginTs() {
         $.ajax({
-		             url: 'https://lr12040717.github.io/api/images.js',
-		            	method: 'GET',
-		            	dataType: 'JSON',
-		            	success: function (res) {
-		                 if (res.code == '0') {
-                 						var userAgent = navigator.userAgent.toLowerCase();
-                 						if (userAgent.match(/(iphone|ipod|android|ios|mobile)/)) {
-                 							   layer.msg(res.msg + "手机端", {
-                 			       offset: '60px',
-                 			       icon: 1
-                 			            			});
-                 						} else {
-                 							layer.msg(res.msg + "电脑端", {
-                 			               				offset: '60px',
-                 			                			icon: 1
-                 			            			});
-                 						}
-		                	}
-		                	else {
-		                    		layer.msg(res.msg);
-		                	}
-		            	},
-		            	error: function () {
-		                	layer.msg("网络请求错误！");
-		            	}
-		        });
-    		});
- 
-    }
+		url: 'https://lr12040717.github.io/api/images.js',
+	       	method: 'GET',
+	       	dataType: 'JSON',
+	       	success: function (res) {
+		        if (res.code == '0') {
+                		var userAgent = navigator.userAgent.toLowerCase();
+                 		if (userAgent.match(/(iphone|ipod|android|ios|mobile)/)) {
+                 			layer.msg(res.msg + "手机端", {
+                 			   	offset: '60px',
+                 			       	icon: 1
+                       			});
+                 		} else {
+                 			layer.msg(res.msg + "电脑端", {
+                 				offset: '60px',
+                 				icon: 1
+                  			});
+     				}
+		        }
+		     	else {
+                   		layer.msg(res.msg);
+	              	}
+           	},
+            	error: function () {
+	               	layer.msg("网络请求错误！");
+          	}
+        });
+    });
     
-    exports('lrAll', {});
+    exports('lrAll', {
+	    loginTs: loginTs
+    });
 });
