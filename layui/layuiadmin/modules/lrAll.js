13100f6d56a92307c4fb9c6add32e9293fba6756
lr12040717 <112813073+lr12040717@users.noreply.github.com>
2024-06-25 15:14:10 +0800

    Update lrAll.js

--- layui/layuiadmin/modules/lrAll.js
+++ layui/layuiadmin/modules/lrAll.js
@@ -1,1 +1,20 @@
+/**
 
+ @Name：layuiAdmin 公共业务
+ 
+ 
+ @License：LPPL
+    
+ */
+ 
+layui.define(function(exports){
+    var $ = layui.$
+    ,layer = layui.layer
+  ,laytpl = layui.laytpl
+  ,setter = layui.setter
+  ,view = layui.view
+  ,admin = layui.admin
+  
+
+  exports('common', {});
+});
