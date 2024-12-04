626e1422aff5ebcecda956306b0465cbfbf1ec21
凌然科技 <112813073+lr12040717@users.noreply.github.com>
2024-12-04 09:56:46 +0800

    Create location.js

---
+++ location/location.js
@@ -0,0 +1,45 @@
+{
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "demo1"
+            },
+            "geometry": {
+                "type": "LineString",
+                "coordinates": [
+                    [
+                        116.41389099999994,
+                        39.914880077046746
+                    ],
+                    [
+                        116.41383099999993,
+                        39.91487007704753
+                    ]
+                ]
+            }
+        },
+    ]
+	"features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "demo2"
+            },
+            "geometry": {
+                "type": "LineString",
+                "coordinates": [
+                    [
+                        116.41389099999994,
+                        39.914880077046746
+                    ],
+                    [
+                        116.41383099999993,
+                        39.91487007704753
+                    ]
+                ]
+            }
+        },
+    ]
+}
